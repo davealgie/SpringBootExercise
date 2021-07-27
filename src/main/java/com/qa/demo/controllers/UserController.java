02308ff9c254924ef649e2558b66af8c08b916d3
davealgie <davidalgie@hotmail.co.uk>
2021-07-27 15:48:14 +0100

    Added Delete and Put methods and Started to edit properties

--- src/main/java/com/qa/demo/controllers/UserController.java
+++ src/main/java/com/qa/demo/controllers/UserController.java
@@ -5,9 +5,13 @@ import java.util.List;
 import java.util.stream.Collectors;
 import javax.validation.Valid;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -28,11 +32,25 @@ public class UserController {
 	
 	}
 	
+	@PutMapping("/{id}")
+	public User updateUser(@PathVariable("id") int id, @RequestBody User user) {
+		for (User a : users) {
+			if (a.getId() == id) {
+				a.setAge(user.getAge());
+				a.setEmail(user.getEmail());
+				a.setName(user.getName());
+				
+				return a;
+			}
+		}
+		return null;
+	}
+	
 	@GetMapping
 	public List<User> getUsers() {
 		return this.users;
 	}
-	
+
 	@GetMapping("/{id}")
 	public User getUserbyId(@PathVariable("id")int id) {
 		for (User user : users) {
@@ -43,6 +61,7 @@ public class UserController {
 		return null;
 	}
 	
+	// localhost:8080/user/email?email=GeorgeEmail@bruh.com
 	@GetMapping("/email")
     public List<User> getUsersByEmail(@RequestParam("email") String email) {
     return users.stream()
@@ -55,5 +74,25 @@ public class UserController {
 		this.users.add(user);
 		return user;
 	}
+	
+	@DeleteMapping("/{id}")
+	public void deleteUser(@PathVariable("id") int id) {
+		for (int i = 0; i < users.size(); i++) {
+			User a = users.get(i);
+			
+			if (a.getId() == id) {
+				this.users.remove(a);
+				break;
+			}
+		}
+	}
+	
+	/*
+	 * @DeleteMapping("/{id}") - This works but throws "status": 500,"error": "Internal Server Error"!
+	 * public void deleteUser(@PathVariable("id") int id) {
+	 * this.users.forEach(a -> { if (a.getId() == id) {  
+	 * this.users.remove(a); } });
+	 * }
+	 */
 
 }
